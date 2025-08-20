e31d71a515bdb7af58c97e38f348cf1c3113b80a
Azis JP <axzisalpha@gmail.com>
2025-08-20 02:42:10 +0000

    [17:15:26.873] Running build in Washington, D.C., USA (East) – iad1 [17:

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDVDF9FHmpJKkVnLqNt0m3cTrFfOm0ueEc",
+  authDomain: "portfoliohub-tgcie.firebaseapp.com",
+  projectId: "portfoliohub-tgcie",
+  storageBucket: "portfoliohub-tgcie.firebasestorage.app",
+  messagingSenderId: "846472260284",
+  appId: "1:846472260284:web:439362a85bfc440764feb9",
+  measurementId: ""
 };
 
 // Initialize Firebase
